64ff980094383907cf1d5892754748c6f2a07433
Peli de Halleux <jhalleux@microsoft.com>
2016-09-05 23:50:32 -0700

    install node.js 6 and build-essential

--- install.sh
+++ install.sh
@@ -3,13 +3,11 @@
 # PXT install for Raspbian
 #
 ###################################
-# remove node.js
-sudo apt-get remove nodejs -y
 # install latest node.js
-curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
 sudo apt-get install -y nodejs
 # install PXT
-sudo npm install -g pxt
+sudo npm install -g build-essential pxt
 # setup microbit
 mkdir -p microbit
 cd microbit
