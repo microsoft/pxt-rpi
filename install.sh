e5752fad71b504c12091647cc07a5d21056fae5f
Peli de Halleux <jhalleux@microsoft.com>
2016-09-05 23:34:15 -0700

    handling over-installation

--- install.sh
+++ install.sh
@@ -6,14 +6,19 @@
 # remove node.js
 sudo apt-get remove nodejs -y
 # install latest node.js
+rm node_latest_armhf.deb
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
 # install PXT
 sudo npm install -g pxt
 # setup microbit
-mkdir microbit
+if [! -d "microbit"]; then
+  mkdir microbit
+fi
 cd microbit
 pxt target microbit
+if [! -d "projects"]; then
 mkdir projects
+fi
 cd projects
 
