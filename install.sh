29b72d922914590197b0ee6054e8726c30d8c033
Peli de Halleux <jhalleux@microsoft.com>
2016-09-05 23:30:50 -0700

    fixed typo in script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get remove nodejs -y
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
 # install PXT
-sudo npm install -g PXT
+sudo npm install -g pxt
 # setup microbit
 mkdir microbit
 cd microbit
