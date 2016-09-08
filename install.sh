32aa3b7fdfc07795df2038930cde42e74ab56d36
Peli de Halleux <jhalleux@microsoft.com>
2016-09-07 23:55:50 -0700

    fixing instructions

--- install.sh
+++ install.sh
@@ -5,13 +5,10 @@
 ###################################
 # install latest node.js
 curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
-sudo apt-get install -y build-essential nodejs
+sudo apt-get install -y build-essential nodejs iceweasel
 # install PXT
 sudo npm install -g pxt
 # setup microbit
 mkdir -p microbit
 cd microbit
 pxt target microbit
-mkdir -p projects
-cd projects
-
