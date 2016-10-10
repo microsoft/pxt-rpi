5a423e1108b41a62ba5daa5a94e0ec7a7193890c
Peli de Halleux <jhalleux@microsoft.com>
2016-10-09 22:24:48 -0700

    don't install iceweasel anymore

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@
 #
 ###################################
 # install latest node.js
+
 curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
-sudo apt-get install -y build-essential nodejs iceweasel
+sudo apt-get install -y build-essential nodejs
 # install PXT
 sudo npm install -g pxt
 # setup microbit
