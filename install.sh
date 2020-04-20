769b4d389f32661e73290ad8cbdcaec87d480d06
Rafael A O Paulucci <rpaulucci3@users.noreply.github.com>
2020-04-20 15:18:38 -0300

    Update node setup file to 10.x; add libudev-dev

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@
 ###################################
 # install latest node.js
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
-sudo apt-get install -y build-essential nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt-get install -y build-essential nodejs libudev-dev
 # install PXT
 sudo npm install -g pxt
 # setup microbit
